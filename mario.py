d3e6ddb4f31afe44fea0cc600bac1ef280d6ba41
SamRod33 <sar325@cornell.edu>
2024-05-10 21:29:00 -0400

    Defined new mario class to wrap mario environment and refactored runner

--- mario.py
+++ mario.py
@@ -1,15 +1,61 @@
 from pettingzoo.atari import mario_bros_v3
 import time
 
-rom_path = '.'
+def mario_env():
+    return MarioEnv()
 
-env = mario_bros_v3.parallel_env(auto_rom_install_path=rom_path, render_mode='human')
-observations, infos = env.reset()
+SEED = 695
 
-while env.agents:
-    time.sleep(0.01)
-    # this is where you would insert your policy
-    actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+class MarioEnv:
+    def __init__(self, seed=SEED) -> None:
+        rom_path = '.'
+        self.env = mario_bros_v3.parallel_env(auto_rom_install_path=rom_path, render_mode='human')
+        self.reset(seed)
+        self.agents = self.env.agents
+        self.num_agents = self.env.num_agents
+        self.possible_agents = self.env.possible_agents
+        self.max_num_agents = self.env.max_num_agents
+        self.observation_spaces = self.env.observation_spaces
+        self.action_spaces = self.env.action_spaces
+        
+    def step(self, actions):
+        return self.env.step(actions)
+    
+    def reset(self, seed=None, options=None):
+        return self.env.reset(seed, options)
+    
+    def render(self):
+        return self.env.render()
+    
+    def close(self):
+        return self.env.close()
+    
+    def state(self):
+        return self.env.state()
+    
+    def observation_space(self, agent_id):
+        return self.env.observation_space(agent_id)
+    
+    def action_space(self, agent_id):
+        return self.env.action_space(agent_id)
+    
+    def reseed(self, seed=SEED):
+        for agent_id in self.agents:
+            self.observation_space(agent_id).seed(seed)
+            self.action_space(agent_id).seed(seed)
+        
+    
+        
+        
+    
+if __name__ == '__main__':
+    env = mario_env()
+    observations, infos = env.reset()
 
-    observations, rewards, terminations, truncations, infos = env.step(actions)
-env.close()
+    while env.agents:
+        time.sleep(0.01)
+        # this is where you would insert your policy
+        actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+
+        observations, rewards, terminations, truncations, infos = env.step(actions)
+    env.close()
--- runner.py
+++ runner.py
@@ -3,8 +3,7 @@ import torch
 import random
 import matplotlib.pyplot as plt
 from a2c import PolicyGradient, ActorCriticPolicyGradient, ValueNet, PolicyNet
-
-seed = 695
+import mario
 
 # Setting the seed to ensure reproducability
 def reseed(seed):
@@ -12,37 +11,33 @@ def reseed(seed):
     random.seed(seed)
     np.random.seed(seed)
 
-reseed(seed)
+def plot_avg_reward(avg_rewards_a2c):
+    # TODO: EDIT
+    iterations = (np.arange(20) + 1) * 10
+    plt.figure(figsize=(10, 6))
+    plt.plot(iterations, avg_rewards_a2c, marker='o', linestyle='-', color='b')
+    plt.title('Advantage Actor-Critic (Part 3) Average Rewards During Training')
+    plt.xlabel('Training Iteration')
+    plt.xticks(range(0, 210, 10))
+    plt.ylabel('Average Reward')
+    plt.savefig('plot-p3.png')
+    plt.show()
 
-# Feel free to use the space below to run experiments and create plots used in your writeup.
-env = gym.make("CartPole-v1")
-env.seed(seed)
-env.action_space.seed(seed)
-env.observation_space.seed(seed)
+if __name__ == '__main__':
+    seed = mario.SEED
+    reseed(seed)
 
-# Feel free to use the space below to run experiments and create plots used in your writeup.
-reseed(seed)
-env.seed(seed)
-env.action_space.seed(seed)
-env.observation_space.seed(seed)
-env.reset()
+    # Feel free to use the space below to run experiments and create plots used in your writeup.
+    env = mario.mario_env()
+    env.reseed()
 
-policy_net_a2c = PolicyNet(env.observation_space.shape[0], env.action_space.n, 128)
-value_net = ValueNet(env.observation_space.shape[0], 128)
+    policy_net_a2c = PolicyNet(env.observation_space.shape[0], env.action_space.n, 128)
+    value_net = ValueNet(env.observation_space.shape[0], 128)
 
-a2c = ActorCriticPolicyGradient(env, policy_net_a2c, value_net, seed)
-avg_rewards_a2c = a2c.train(num_iterations=200, batch_size=10, gamma=0.99, lr=0.01)
+    a2c = ActorCriticPolicyGradient(env, policy_net_a2c, value_net, seed)
+    avg_rewards_a2c = a2c.train(num_iterations=200, batch_size=10, gamma=0.99, lr=0.01)
 
-# visualize(algorithm=a2c, video_name="a2c")
-print(f'\nACTOR-CRITIC Expected Reward: {a2c.evaluate()}')
+    # visualize(algorithm=a2c, video_name="a2c")
+    print(f'\nACTOR-CRITIC Expected Reward: {a2c.evaluate()}')
 
-iterations = (np.arange(20) + 1) * 10
-plt.figure(figsize=(10, 6))
-plt.plot(iterations, avg_rewards_a2c, marker='o', linestyle='-', color='b')
-plt.title('Advantage Actor-Critic (Part 3) Average Rewards During Training')
-plt.xlabel('Training Iteration')
-plt.xticks(range(0, 210, 10))
-plt.ylabel('Average Reward')
-plt.savefig('plot-p3.png')
-
-plt.show()
+    
